3b2fff339f3a4ddaf24dc18ef74492a05a6c3b09
Elizabeth <andikoelizabeth@gmail.com>
2018-11-30 13:56:26 +0300

    include changes in function

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
-function triangle(){
 var a = parseInt(prompt("Enter side a"));
 var b = parseInt(prompt("Enter side b"));
 var c = parseInt(prompt("Enter side c"));
-if ((a===b) && (b===c)){
+if (a===b && b===c && a===c){
 alert ("Equilateral")
 }
-if ((a==b) || (b==c) || (a==c)) {
+if (a==b || b==c || a==c) {
 alert ("Isosceles")
 }
-if ((a!==b) && (b!==c) && (a!==c)){
+if (a!==b && b!==c && a!==c){
 alert ("Scalene")
 }
 else {
 alert ("Not a Triangle")
-}
 }
