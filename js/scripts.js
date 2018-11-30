26f92baf45033d30bd9a554b92eee45684c8d4d4
Elizabeth <andikoelizabeth@gmail.com>
2018-11-30 12:12:38 +0300

    add control flow function in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,11 @@ function triangle(){
 var a = parseInt(prompt("Enter side a"));
 var b = parseInt(prompt("Enter side b"));
 var c = parseInt(prompt("Enter side c"));
+if ((a===b) && (b===c)){
+alert ("Equilateral")
+}
+if ((a==b) || (b==c) || (a==c)) {
+alert ("Isosceles")
+}
+
 }
